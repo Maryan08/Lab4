c7ae007a691ba1a75cdc8b66bb53a55efa163d2a
Maryan08 <mar08khymych@gmail.com>
2020-05-15 16:19:50 +0300

    First

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            float R = 55;
+            string result = OOP_Lab_4._1.Program.StudentRating(R);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
